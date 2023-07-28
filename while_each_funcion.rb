1453876ac417be6e7f571f4e6634a57f9af3ef00
Fabian Gutierrez <fabiandavidgutierrez91@gmail.com>
2023-07-27 19:45:23 -0500

    corrección

--- while_each_funcion.rb
+++ while_each_funcion.rb
@@ -1,13 +1,13 @@
 # Imprima los n números primos contenidos en un intervalo (por ejemplo los números primos del 1 al 10)
 
 def es_primo(a)
+    contador_residuo = 0
     (1..a).each do |i|
-        contador_residuo = 0
         if a % i == 0
             contador_residuo += 1
         end
-    return contador_residuo == 2
     end
+    return contador_residuo == 2
 end
 
 puts "Ingrese inicio del intervalo: "
@@ -15,6 +15,8 @@ inicio = gets.to_i
 
 puts "Ingrese fin del intervalo: "
 fin = gets.to_i
+
+puts "Los números primos del intervalo #{inicio}-#{fin} son: "
 
 while fin > inicio
     if es_primo(inicio)
